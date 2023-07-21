b4e3170395fa0a5fed34b3e9122c586200f342be
Lakunta Tillis <kt_tillis@hotmail.com>
2023-07-20 19:16:41 -0500

    Update __name@dasherize__.component.spec.ts

--- axe-scanner/src/testing-library/files/__name@dasherize__/__name@dasherize__.component.spec.ts
+++ axe-scanner/src/testing-library/files/__name@dasherize__/__name@dasherize__.component.spec.ts
@@ -1,18 +1,37 @@
-import { render, screen } from '@testing-library/angular';
+import { axe, toHaveNoViolations } from "jasmine-axe";
+import { TestBed } from "@angular/core/testing";
 import { <%= classify(name) %>Component } from './<%= dasherize(name) %>.component';
 
-describe('<%= classify(name) %>Component', () => {
-  const renderOptions = {
-    imports: [],
-  };
-
-  it('should create component', async () => {
-    const rr = await render(<%= classify(name) %>Component, renderOptions);
-    expect(rr.fixture.componentInstance).toBeTruthy();
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        <%= classify(name) %>Component
+      ],
+    }).compileComponents();
   });
 
-  it('should render content', async () => {
-    await render(<%= classify(name) %>Component, renderOptions);
-    expect(screen.getByText('<%= dasherize(name) %> works!')).not.toBeNull();
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(<%= classify(name) %>Component);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'testingApp'`, () => {
+    const fixture = TestBed.createComponent(<%= classify(name) %>Component);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('testingApp');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(<%= classify(name) %>Component);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('testingApp app is running!');
+  });
+
+  it("should pass accessibility test", async () => {
+    const fixture = TestBed.createComponent(<%= classify(name) %>Component);
+    expect(await axe(fixture.nativeElement)).toHaveNoViolations();
   });
 });
